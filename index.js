6237e949caa90da46cb8f19578f185df203db7bf
ebarroso2214 <ebarroso2214@gmail.com>
2023-03-21 19:15:08 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('*',(req,res) =>{
+    res.render('error404')
+})
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page </h1>')
